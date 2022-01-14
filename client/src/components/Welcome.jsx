5be5cef4893c65d589ecff32ecf9705dad5f0359
allan-moya <allan.s.moya@gmail.com>
2022-01-13 19:19:07 -0800

    first commit

---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+
+
+const Welcome = () => {
+    return (
+        <div className="flex w-full justify-center items-center">
+            <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+
+            </div>
+        </div>
+    )
+}
+
+export default Welcome
